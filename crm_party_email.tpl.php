c7743881d8496bf96a15877bd65f8e64ae7d534c
Robert Mumford <rlmumford@googlemail.com>
2011-08-26 15:33:33 +0100

    Used the tribes_contact code to make a working contact/party entity, now we can fiddle with it.

---
+++ crm_party_email.tpl.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * tpl for handling contact specific email
+ *
+ * 
+ * Helper variables:
+ * - $contact: The Model object this status is derived from
+ */
+?>
+
+<div class="tribes_contact_email">
+  <?php print "<strong>Email:</strong> " . $tribes_contact_email; ?>
+</div>
+  
