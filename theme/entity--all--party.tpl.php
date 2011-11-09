f035f1c8f15e4bcf015979ff700f8c9de1208ba4
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2011-11-09 22:00:54 +0000

    Moved entity tpl for party view to base module so it can handle all attached entities.

--- theme/entity--all--party.tpl.php
+++ theme/entity--all--party.tpl.php
@@ -2,22 +2,22 @@
 
 /**
  * @file
- * Theme implementation for profiles shown within a party.
+ * Theme implementation for entities shown within a party.
  *
  * Available variables:
  * - $content: An array of comment items. Use render($content) to print them all, or
  *   print a subset such as render($content['field_example']). Use
  *   hide($content['field_example']) to temporarily suppress the printing of a
  *   given element.
- * - $title: The (sanitized) profile type label.
- * - $url: The URL to view the current profile.
- * - $page: TRUE if this is the main view page $url points too.
+ * - $title: The (sanitized) entity label.
+ * - $url: Direct url of the current entity if specified.
+ * - $page: Flag for the full page state.
  * - $classes: String of classes that can be used to style contextually through
  *   CSS. It can be manipulated through the variable $classes_array from
  *   preprocess functions. By default the following classes are available, where
  *   the parts enclosed by {} are replaced by the appropriate values:
- *   - entity-profile
- *   - profile-{TYPE}
+ *   - entity-{ENTITY_TYPE}
+ *   - {ENTITY_TYPE}-{BUNDLE}
  *
  * Other variables:
  * - $classes_array: Array of html class attribute values. It is flattened
