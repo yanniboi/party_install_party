3ff8f0d170d06df8af4902dfedadf046db9d69b9
Robert Mumford <rlmumford@googlemail.com>
2011-12-09 11:57:37 +0000

    Issue 1359406: Changed Module Name to Party - Added New Files

---
+++ _party_email.tpl.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * tpl for handling contact specific email
+ *
+ * 
+ * Helper variables:
+ * - $contact: The Model object this status is derived from
+ */
+?>
+
+<div class="party_email">
+  <?php print "<strong>Email:</strong> " . $party_email; ?>
+</div>
+  
